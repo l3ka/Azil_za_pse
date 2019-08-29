03f9d21f7556f5f6cb7dd85bad2a0749937fdc9b
StefanErceg <stefanerceg95@gmail.com>
2019-08-29 17:57:56 +0200

    male izmjene na GUI-u

--- src/GUI/admin/add_change_account/AddAndChangeAccountController.java
+++ src/GUI/admin/add_change_account/AddAndChangeAccountController.java
@@ -1,5 +1,6 @@
 package GUI.admin.add_change_account;
 
+import GUI.alert_box.AlertBoxForm;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -30,7 +31,85 @@ public class AddAndChangeAccountController {
         this.stage = stage;
     }
 
+    public void saveAccount() {
+        if (checkName() && checkQualifications() && checkPlaceOfResidence() &&
+                checkIdentificationNumber() && checkPhoneNumber()) {
+            try {
+                new AlertBoxForm("Nalog je uspješno sačuvan!").display();
+            } catch(Exception exception) {
 
+            }
+        }
+    }
 
+    private boolean checkName() {
+        if ("".equals(nameTextField.getText()) || "".equals(surnameTextField.getText())) {
+            try {
+                new AlertBoxForm("Unos za polja ime i prezime nije odgovarajući!").display();
+            } catch (Exception exception) {
+
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkQualifications() {
+        if ("".equals(qualificationsTextField.getText())) {
+            try {
+                new AlertBoxForm("Unos za polje stručna sprema nije odgovarajući!").display();
+            } catch (Exception exception) {
+
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkIdentificationNumber() {
+        String identificationNumber = identificationNumberTextField.getText();
+        if ("".equals(identificationNumber) || !checkNumber(identificationNumber)) {
+            try {
+                new AlertBoxForm("Unos za polje JMB nije odgovarajući!").display();
+            } catch (Exception exception) {
+
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPlaceOfResidence() {
+        if ("".equals(placeOfResidenceTextField.getText())) {
+            try {
+                new AlertBoxForm("Unos za polje mjesto stanovanja nije odgovarajući!").display();
+            } catch (Exception exception) {
+
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkPhoneNumber() {
+        if ("".equals(phoneNumberTextField.getText())) {
+            try {
+                new AlertBoxForm("Unos za polje broj telefona nije odgovarajući!").display();
+            } catch (Exception exception) {
+
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean checkNumber(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch(NumberFormatException e) {
+            return false;
+        }
+    }
 
 }
