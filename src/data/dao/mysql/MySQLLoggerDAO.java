91da818c2617af0e02c84922403f55f02dc28a11
Darko Lekic <lekic.d7@gmail.com>
2019-09-08 19:09:56 +0200

    add logger v1.1

--- src/data/dao/mysql/MySQLLoggerDAO.java
+++ src/data/dao/mysql/MySQLLoggerDAO.java
@@ -1,4 +1,31 @@
 package data.dao.mysql;
 
-public class MySQLLoggerDAO {
+import data.dao.LoggerDAO;
+import data.dto.LoggerDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class MySQLLoggerDAO implements LoggerDAO {
+
+    @Override
+    public void insert(LoggerDTO loggerDTO) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        String query = "INSERT INTO logger values (?, ?, ?)";
+        try{
+            conn = ConnectionPool.getInstance().checkOut();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, null);
+            ps.setString(2, loggerDTO.getUsername());
+            ps.setString(3, loggerDTO.getDescription());
+            ps.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            ConnectionPool.getInstance().checkIn(conn);
+            DBUtilities.getInstance().close(ps);
+        }
+    }
+
 }
