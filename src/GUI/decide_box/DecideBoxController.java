874e275e2692469f35a53637dbf32acb2f00b995
StefanErceg <stefanerceg95@gmail.com>
2019-09-14 17:12:09 +0200

    decideBox

---
+++ src/GUI/decide_box/DecideBoxController.java
@@ -0,0 +1,31 @@
+package GUI.decide_box;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class DecideBoxController {
+
+    @FXML
+    private Label messageLabel;
+    private Stage stage;
+    private boolean decision = false;
+
+    public void initialize(Stage stage, String message) {
+        this.stage = stage;
+         messageLabel.setText(message);
+    }
+
+    public void yesClicked() {
+        this.stage.close();
+        this.decision = true;
+    }
+
+    public void noClicked() {
+        this.stage.close();
+    }
+
+    public boolean getDecision() {
+        return this.decision;
+    }
+}
