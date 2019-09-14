5f0295a577507819265dc940a2f5604b636f0ed4
Darko Lekic <lekic.d7@gmail.com>
2019-09-14 18:12:36 +0200

    refactor and review of /preview

--- src/GUI/preview/accounts_preview/AccountsForm.java
+++ src/GUI/preview/accounts_preview/AccountsForm.java
@@ -1,5 +1,6 @@
 package GUI.preview.accounts_preview;
 
+import data.dto.EmployeeDTO;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
@@ -9,7 +10,7 @@ import java.io.File;
 
 public class AccountsForm {
 
-    public void display() throws Exception {
+    public void display(EmployeeDTO employee) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("accountsPreview.fxml"));
         Stage stage = new Stage();
         stage.getIcons().add(new Image("file:" + "src" + File.separator + "resources" + File.separator + "reportImages" + File.separator + "employee.png"));
@@ -19,6 +20,7 @@ public class AccountsForm {
         stage.setMinHeight(700);
         stage.setMinWidth(1300);
         AccountsController controller = loader.getController();
+        controller.setEmployee(employee);
         controller.initialize(stage);
         stage.showAndWait();
     }
