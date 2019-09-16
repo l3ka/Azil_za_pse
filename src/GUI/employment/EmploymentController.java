ba4f7eab6ad027dbd7647375144c7b7b4e04ecc0
Milicaaa95 <milicama.95@gmail.com>
2019-09-16 10:43:02 +0200

    PROCITAJTE OPIS OVAJ
    sredjene sve glavne forme, u login sam dodala button zatvori :D
    kod forme za zaposljavanje iz nekog razloga NE RADI prikaz o ugovoru, a isto sam odradila kao sto je odradjeno za udomljavanje, istom logikom
    Popravicu danas :D

--- src/GUI/employment/EmploymentController.java
+++ src/GUI/employment/EmploymentController.java
@@ -2,75 +2,66 @@ package GUI.employment;
 
 import GUI.alert_box.AlertBoxForm;
 import data.dto.EmployeeContractDTO;
+import data.dto.EmployeeDTO;
+import data.dto.EmploymentContractDTO;
 import data.dto.LoggerDTO;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import util.AzilUtilities;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class EmploymentController {
 
     @FXML
-    private TableView<EmployeeContractDTO> employmentTableView;
+    private TableView<EmployeeDTO> employeesTableView;
     @FXML
-    private TableColumn<EmployeeContractDTO, String> columnJMB;
+    private Label positionLabel;
     @FXML
-    private TableColumn<EmployeeContractDTO, String> columnName;
+    private Label salaryLabel;
     @FXML
-    private TableColumn<EmployeeContractDTO, String> columnSurname;
-    @FXML
-    private TableColumn<EmployeeContractDTO, String> columnAddress;
-    @FXML
-    private TableColumn<EmployeeContractDTO, String> columnPhone;
-    @FXML
-    private TableColumn<EmployeeContractDTO, String> columnSalary;
-    @FXML
-    private TableColumn<EmployeeContractDTO, String> columnPosition;
+    private Label dateFromLabel;
+
 
     private Stage stage;
+    private List<EmployeeDTO> listOfEmployees = new ArrayList<>();
     private List<EmployeeContractDTO> listOfEmployments;
 
     public void initialize(Stage stage) {
         try {
             this.stage = stage;
 
-            /*employmentTableView.getColumns().get(7).setCellValueFactory(new PropertyValueFactory<>("from"));
-            employmentTableView.getColumns().get(8).setCellValueFactory(new PropertyValueFactory<>("to"));
-            columnJMB.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEmployee().getJMB()));
-            columnName.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEmployee().getName()));
-            columnSurname.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEmployee().getSurname()));
-            columnAddress.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEmployee().getResidenceAddress()));
-            columnPhone.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEmployee().getTelephoneNumber()));
-            columnSalary.setCellValueFactory(data -> new SimpleStringProperty(Double.toString(data.getValue().getEmploymentContract().getSalary())));
-            columnPosition.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getEmploymentContract().getPosition()));
+            employeesTableView.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("JMB"));
+            employeesTableView.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("name"));
+            employeesTableView.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("surname"));
+            employeesTableView.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("residenceAddress"));
+            employeesTableView.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("telephoneNumber"));
+            employeesTableView.getColumns().get(5).setCellValueFactory(new PropertyValueFactory<>("qualifications"));
 
-            displayEmployments();*/
+            displayEmployees();
+            listOfEmployments = AzilUtilities.getDAOFactory().getEmployeeContractDAO().getAllEmployeeContract();
+
+            employeesTableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+                int contractId = listOfEmployments.stream().
+                        filter(employeeContractDTO -> employeeContractDTO.getJmbEmployee().equals(newValue.getJMB()) && employeeContractDTO.getTo() == null).
+                        findAny().get().getIdEmploymentContract();
+                EmploymentContractDTO employmentContract = AzilUtilities.getDAOFactory().getEmploymentContractDAO().getById(contractId);
+                positionLabel.setText(employmentContract.getPosition());
+                salaryLabel.setText("" + employmentContract.getSalary());
+                for(EmployeeContractDTO employeeContract : listOfEmployments) {
+                    if(employeeContract.getIdEmploymentContract() == contractId) {
+                        dateFromLabel.setText(employeeContract.getFrom().toString());
+                    }
+                }
+            });
         } catch(Exception ex) {
             AzilUtilities.getDAOFactory().getLoggerDAO().insert(new LoggerDTO("EmploymentController - initialize", new Date(Calendar.getInstance().getTime().getTime()), ex.fillInStackTrace().toString()));
-        }
-    }
-
-
-    public void editEmployment() {
-        try {
-            if(checkSelectedEmployment()) {
-
-            }
-        } catch(Exception ex) {
-            AzilUtilities.getDAOFactory().getLoggerDAO().insert(new LoggerDTO("EmploymentController - editEmployment", new Date(Calendar.getInstance().getTime().getTime()), ex.fillInStackTrace().toString()));
-        }
-    }
-
-    public void displayAllEmployments() {
-        try {
-            displayEmployments();
-        } catch(Exception ex) {
-            AzilUtilities.getDAOFactory().getLoggerDAO().insert(new LoggerDTO("EmploymentController - displayAllEmployments", new Date(Calendar.getInstance().getTime().getTime()), ex.fillInStackTrace().toString()));
         }
     }
 
@@ -82,37 +73,16 @@ public class EmploymentController {
         }
     }
 
-    private boolean checkSelectedEmployment() {
-        try {
-            if(employmentTableView.getSelectionModel().getSelectedItem() == null) {
-                displayAlertBox("Nije izabran zaposleni i njegov ugovor!");
-                return false;
-            }
-            return true;
-        } catch(Exception ex) {
-            AzilUtilities.getDAOFactory().getLoggerDAO().insert(new LoggerDTO("EmploymentController - checkSelectedEmployment", new Date(Calendar.getInstance().getTime().getTime()), ex.fillInStackTrace().toString()));
-            return false;
-        }
-    }
+    private void displayEmployees() {
+        employeesTableView.getItems().clear();
+        employeesTableView.refresh();
 
-    private void displayEmployments() {
-        try {
-            employmentTableView.getItems().clear();
-            employmentTableView.refresh();
-            listOfEmployments = AzilUtilities.getDAOFactory().getEmployeeContractDAO().getAllEmployeeContract();
-            for(EmployeeContractDTO employeeContract : listOfEmployments) {
-                employmentTableView.getItems().add(employeeContract);
-            }
-        } catch(Exception ex) {
-            AzilUtilities.getDAOFactory().getLoggerDAO().insert(new LoggerDTO("EmploymentController - displayEmployments", new Date(Calendar.getInstance().getTime().getTime()), ex.fillInStackTrace().toString()));
-        }
-    }
+        listOfEmployees.addAll(AzilUtilities.getDAOFactory().getAdministratorDAO().adminstartors());
+        listOfEmployees.addAll(AzilUtilities.getDAOFactory().getServantDAO().servants());
+        listOfEmployees.addAll(AzilUtilities.getDAOFactory().getVeterinarinaDAO().veterinarians());
 
-    private void displayAlertBox(String content) {
-        try {
-            new AlertBoxForm(content).display();
-        } catch(Exception ex) {
-            AzilUtilities.getDAOFactory().getLoggerDAO().insert(new LoggerDTO("EmploymentController - displayAlertBox", new Date(Calendar.getInstance().getTime().getTime()), ex.fillInStackTrace().toString()));
+        for(EmployeeDTO employee : listOfEmployees) {
+            employeesTableView.getItems().add(employee);
         }
     }
 
