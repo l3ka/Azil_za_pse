f707cd663a3969357a8bef19eeabbf76af3d5948
Darko Lekic <lekic.d7@gmail.com>
2019-09-18 00:37:11 +0200

    add dog delete

--- resources/database/data.sql
+++ resources/database/data.sql
@@ -1,5 +1,6 @@
 use azil;
 
+-- Zaposleni
 insert into zaposleni values (1234567891234, "Milica", "Matic", "milica", "f50ccb98908dd89ef400372d10750d4c506bebdc34c8141e11d2fb962ebb1b5d", "Srednja strucna sprema", "Banja Luka", 065123456);
 insert into zaposleni values (1234567891235, "Stefan", "Erceg", "stefan", "52518386cc33022de894fa0af047bd62666a63c2a6a6e86650e26955058c5acf", "Srednja strucna sprema", "Banja Luka", 065123457);
 insert into zaposleni values (1234567891236, "Rade", "Culum", "rade", "0d0bf087d6c879ac9aa426297b2e90528f25f005f82e2aed81e0e15066bfe322", "Srednja strucna sprema", "Banja Luka", 065123458);
@@ -7,7 +8,7 @@ insert into zaposleni values (1234567891237, "Milan", "Markovic", "milan", "efee
 insert into zaposleni values (1234567891238, "Jelena", "Aleksic", "jelena", "358ccf83f867e8147663680f52d951dfa9b21a8b7d59b3b46eb917bdf5952c92", "Srednja strucna sprema", "Banja Luka", 065123455);
 insert into zaposleni values (1234567891239, "Darko", "Lekic", "darko", "fe79e0ac4b7db16d59a67be682f0c2e85e24241cccbb7a6303446e7105362bcc", "Srednja strucna sprema", "Laktasi", 065123454);
 
-
+-- Ugovor o Radu
 insert into ugovororadu values (null, "Sluzbenik", 1, 1250);
 insert into ugovororadu values (null, "Sluzbenik", 1, 1550);
 insert into ugovororadu values (null, "Veterinar", 1, 1350);
@@ -15,7 +16,7 @@ insert into ugovororadu values (null, "Veterinar", 1, 1270);
 insert into ugovororadu values (null, "Administrator", 1, 1294);
 insert into ugovororadu values (null, "Administrator", 1, 2194);
 
-
+-- Ugovori zaposlenika
 insert into zaposleni_ugovor values ("2019-08-01", 1234567891234, 1, null);
 insert into zaposleni_ugovor values ("2019-09-01", 1234567891235, 2, null);
 insert into zaposleni_ugovor values ("2019-01-01", 1234567891236, 3, null);
@@ -23,69 +24,35 @@ insert into zaposleni_ugovor values ("2015-08-01", 1234567891237, 4, null);
 insert into zaposleni_ugovor values ("2019-06-01", 1234567891238, 5, null);
 insert into zaposleni_ugovor values ("2016-08-01", 1234567891239, 6, null);
 
-
+-- Sluzbenici
 insert into sluzbenik values (1234567891234);
 insert into sluzbenik values (1234567891235);
 
+-- Veterinari 
 insert into veterinar values (1234567891236);
 insert into veterinar values (1234567891237);
 
-
+-- Administratori
 insert into administrator values (1234567891238);
 insert into administrator values (1234567891239);
 
+-- Psi
+insert into pas values (null, "Pas1", "M", "Terijer", '2017-01-07', 21, 19.3, null, default, 0);
+insert into pas values (null, "Pas2", "M", "Buldog", '2018-02-10', 11, 12.5, null, default, 0);
+insert into pas values (null, "Pas3", "Ž", "Terijer", '2019-07-20', 14, 17.3, null, default, 0);
+insert into pas values (null, "Pas4", "M", "Buldog", '2017-10-20', 52, 37.3, null, default, 0);
+insert into pas values (null, "Pas5", "Ž", "Terijer", '2018-11-1', 48, 26.3, null, default, 0);
+insert into pas values (null, "Pas6", "M", "Haski", '2019-02-11', 25, 12.33, null, default, 0);
+insert into pas values (null, "Pas7", "Ž", "Terijer", '2017-03-04', 45, 13.3, null, default, 0);
+insert into pas values (null, "Pas8", "M", "Ovcar", '2018-04-21', 65, 67.3, null, default, 0);
+insert into pas values (null, "Pas9", "Ž", "Terijer", '2017-05-28', 19, 9.3, null, default, 0);
+insert into pas values (null, "Pas10", "M", "Ovcar", '2019-03-19', 64, 34.3, null, default, 0);
+insert into pas values (null, "Pas11", "M", "Ovcar", '2019-03-19', 13, 7.32, null, 1, 0);
+insert into pas values (null, "Pas12", "Ž", "Terijer", '2019-03-19', 13, 17.3, null, 1, 0);
+insert into pas values (null, "Pas13", "M", "Haski", '2019-03-19', 24, 7.33, null, 1, 0);
+insert into pas values (null, "Pas14", "M", "Labrador", '2019-03-19', 24, 7.33, null, default, 0);
 
-insert into pas values (null, "Pas1", "M", "Terijer", '2017-01-07', 21, 19.3, null, default);
-insert into pas values (null, "Pas2", "M", "Buldog", '2018-02-10', 11, 12.5, null, default);
-insert into pas values (null, "Pas3", "Ž", "Terijer", '2019-07-20', 14, 17.3, null, default);
-insert into pas values (null, "Pas4", "M", "Buldog", '2017-10-20', 52, 37.3, null, default);
-insert into pas values (null, "Pas5", "Ž", "Terijer", '2018-11-1', 48, 26.3, null, default);
-insert into pas values (null, "Pas6", "M", "Haski", '2019-02-11', 25, 12.33, null, default);
-insert into pas values (null, "Pas7", "Ž", "Terijer", '2017-03-04', 45, 13.3, null, default);
-insert into pas values (null, "Pas8", "M", "Ovcar", '2018-04-21', 65, 67.3, null, default);
-insert into pas values (null, "Pas9", "Ž", "Terijer", '2017-05-28', 19, 9.3, null, default);
-insert into pas values (null, "Pas10", "M", "Ovcar", '2019-03-19', 13, 7.3, null, default);
-insert into pas values (null, "Pas11", "Ž", "Rasa1", '2017-04-03', 52, 27.3, null, default);
-insert into pas values (null, "Pas12", "M", "Terijer", '2019-01-20', 83, 142.3, null, default);
-insert into pas values (null, "Pas13", "Ž", "Pudlica", '2018-11-08', 23, 142.3, null, default);
-insert into pas values (null, "Pas14", "M", "Ovcar", '2019-03-17', 54, 34.3, null, default);
-insert into pas values (null, "Pas15", "M", "Haski", '2016-04-02', 63, 57.3, null, default);
-insert into pas values (null, "Pas16", "Ž", "Buldog", '2015-02-20', 54, 13.3, null, default);
-insert into pas values (null, "Pas17", "Ž", "Ovcar", '2014-08-07', 12, 72.3, null, default);
-insert into pas values (null, "Pas18", "M", "Haski", '2016-03-15', 8, 12.3, null, default);
-insert into pas values (null, "Pas19", "Ž", "Buldog", '2013-07-04', 4, 132.3, null, default);
-insert into pas values (null, "Pas20", "Ž", "Haski", '2012-12-14', 15, 62.3, null, default);
-insert into pas values (null, "Pas21", "M", "Terijer", '2017-01-07', 21, 19.3, null, default);
-insert into pas values (null, "Pas22", "M", "Buldog", '2018-02-10', 11, 12.5, null, default);
-insert into pas values (null, "Pas23", "Ž", "Terijer", '2019-07-20', 14, 17.3, null, default);
-insert into pas values (null, "Pas24", "M", "Buldog", '2017-10-20', 52, 37.3, null, default);
-insert into pas values (null, "Pas25", "Ž", "Terijer", '2018-11-1', 48, 26.3, null, default);
-insert into pas values (null, "Pas26", "M", "Haski", '2019-02-11', 25, 12.33, null, default);
-insert into pas values (null, "Pas27", "Ž", "Terijer", '2017-03-04', 45, 13.3, null, default);
-insert into pas values (null, "Pas28", "M", "Ovcar", '2018-04-21', 65, 67.3, null, default);
-insert into pas values (null, "Pas29", "Ž", "Terijer", '2017-05-28', 19, 9.3, null, default);
-insert into pas values (null, "Pas30", "M", "Ovcar", '2019-03-19', 13, 7.3, null, default);
-insert into pas values (null, "Pas31", "Ž", "Rasa1", '2017-04-03', 52, 27.3, null, default);
-insert into pas values (null, "Pas32", "M", "Terijer", '2019-01-20', 83, 142.3, null, default);
-insert into pas values (null, "Pas33", "Ž", "Pudlica", '2018-11-08', 23, 142.3, null, default);
-insert into pas values (null, "Pas34", "M", "Ovcar", '2019-03-17', 54, 34.3, null, default);
-insert into pas values (null, "Pas35", "M", "Haski", '2016-04-02', 63, 57.3, null, default);
-insert into pas values (null, "Pas36", "Ž", "Buldog", '2015-02-20', 54, 13.3, null, default);
-insert into pas values (null, "Pas37", "Ž", "Ovcar", '2014-08-07', 12, 72.3, null, default);
-insert into pas values (null, "Pas38", "M", "Haski", '2016-03-15', 8, 12.3, null, default);
-insert into pas values (null, "Pas39", "Ž", "Buldog", '2013-07-04', 4, 132.3, null, default);
-insert into pas values (null, "Pas40", "Ž", "Haski", '2012-12-14', 15, 62.3, null, default);
-insert into pas values (null, "Pas41", "Ž", "Rasa1", '2017-04-03', 52, 27.3, null, default);
-insert into pas values (null, "Pas42", "M", "Terijer", '2019-01-20', 83, 142.3, null, default);
-insert into pas values (null, "Pas43", "Ž", "Pudlica", '2018-11-08', 23, 142.3, null, default);
-insert into pas values (null, "Pas44", "M", "Ovcar", '2019-03-17', 54, 34.3, null, default);
-insert into pas values (null, "Pas45", "M", "Haski", '2016-04-02', 63, 57.3, null, default);
-insert into pas values (null, "Pas46", "Ž", "Buldog", '2015-02-20', 54, 13.3, null, default);
-insert into pas values (null, "Pas47", "Ž", "Ovcar", '2014-08-07', 12, 72.3, null, default);
-insert into pas values (null, "Pas48", "M", "Haski", '2016-03-15', 8, 12.3, null, default);
-insert into pas values (null, "Pas49", "Ž", "Buldog", '2013-07-04', 4, 132.3, null, default);
-insert into pas values (null, "Pas50", "Ž", "Haski", '2012-12-14', 15, 62.3, null, default);
-
+-- Nalazi
 insert into nalaz values (null, "2019-08-08", "Alergija", "1234567891236", 1);
 insert into nalaz values (null, "2019-09-08", "Alergija", "1234567891237", 2);
 insert into nalaz values (null, "2019-05-08", "Alergija", "1234567891236", 3);
@@ -95,14 +62,14 @@ insert into nalaz values (null, "2019-08-06", "Alergija", "1234567891237", 6);
 insert into nalaz values (null, "2019-01-08", "Alergija", "1234567891236", 7);
 insert into nalaz values (null, "2019-08-01", "Alergija", "1234567891237", 8);
 insert into nalaz values (null, "2019-02-08", "Infekcija oka", "1234567891236", 9);
-insert into nalaz values (null, "2019-04-02", "Infekcija oka", "1234567891236", 11);
-insert into nalaz values (null, "2019-03-03", "Infekcija oka", "1234567891236", 12);
-insert into nalaz values (null, "2019-08-08", "Infekcija oka", "1234567891236", 13);
-insert into nalaz values (null, "2019-09-09", "Infekcija oka", "1234567891236", 14);
-insert into nalaz values (null, "2019-08-04", "Infekcija oka", "1234567891236", 15);
-insert into nalaz values (null, "2019-04-08", "Infekcija oka", "1234567891236", 15);
+insert into nalaz values (null, "2019-04-02", "Infekcija oka", "1234567891236", 10);
+insert into nalaz values (null, "2019-03-03", "Infekcija oka", "1234567891236", 2);
+insert into nalaz values (null, "2019-08-08", "Infekcija oka", "1234567891236", 3);
+insert into nalaz values (null, "2019-09-09", "Infekcija oka", "1234567891236", 4);
+insert into nalaz values (null, "2019-08-04", "Infekcija oka", "1234567891236", 5);
+insert into nalaz values (null, "2019-04-08", "Infekcija oka", "1234567891236", 5);
 
-
+-- Lijekovi
 insert into lijek values (0, 'Paracetamol', null, 52);
 insert into lijek values (0, 'Amoksicilin', null, 10);
 insert into lijek values (0, 'Brufen', null, 5);
@@ -120,7 +87,7 @@ insert into lijek values (0, 'Kalcijev karbona', null, 64);
 insert into lijek values (0, 'Meglimid', null, 35);
 insert into lijek values (0, 'Lanzum', null, 25);
 
-
+-- Udomitelji 
 insert into udomitelj values ('1507988102015', 'Jelena', 'Miletic', 'Banja Luka', '065/722-980');
 insert into udomitelj values ('2001991107013', 'Marko', 'Markovic', 'Gradiska', '066/471-352');
 insert into udomitelj values ('1507989512015', 'Jelena', 'Markovic', 'Banja Luka', '065/412-456');
@@ -133,31 +100,30 @@ insert into udomitelj values ('2001914707853', 'Zeljko', 'Lojic', 'Prijedor', '0
 insert into udomitelj values ('2001993697853', 'Bojan', 'Petrusic', 'Kotor Varos', '066/963-951');
 insert into udomitelj values ('2001991101233', 'Nemanja', 'Vrljanovic', 'Kotor Varos', '066/147-654');
 
-insert into kavez values (null, "Kavez1", 3, 3);
-insert into kavez values (null, "Kavez2", 4, 4);
-insert into kavez values (null, "Kavez3", 3, 3);
-insert into kavez values (null, "Kavez4",4,  4);
-insert into kavez values (null, "Kavez5", 3, 3);
-insert into kavez values (null, "Kavez6", 4, 4);
-insert into kavez values (null, "Kavez7", 10, 10);
-insert into kavez values (null, "Kavez8", 10, 10);
-insert into kavez values (null, "Kavez9", 7, 7);
-insert into kavez values (null, "Kavez10", 5, 5);
-insert into kavez values (null, "Kavez11", 1, 1);
-insert into kavez values (null, "Kavez12", 1, 1);
-insert into kavez values (null, "Kavez13", 1, 1);
-insert into kavez values (null, "Kavez14", 1, 1);
-insert into kavez values (null, "Kavez15", 7, 7);
-insert into kavez values (null, "Kavez16", 2, 2);
-insert into kavez values (null, "Kavez17", 2, 2);
-insert into kavez values (null, "Kavez18", 2, 2);
-insert into kavez values (null, "Kavez19", 2, 2);
-insert into kavez values (null, "Kavez20", 7, 7);
-insert into kavez values (null, "Kavez21", 5, 5);
-insert into kavez values (null, "Kavez22", 5, 5);
-insert into kavez values (null, "Kavez23", 9, 9);
-insert into kavez values (null, "Kavez24", 9, 9);
-insert into kavez values (null, "Kavez25", 12, 12);
-insert into kavez values (null, "Kavez26", 12, 12);
+-- Psi kod udomitelja
+insert into udomljavanjepsa values ("2019-09-09", 11, '1507988102015', null);
+insert into udomljavanjepsa values ("2019-09-09", 12, '2001991107013', null);
+insert into udomljavanjepsa values ("2019-09-09", 13, '1507989512015', null);
+
+-- Kavezi
+insert into kavez values (null, "Kavez1", 1, 3);
+insert into kavez values (null, "Kavez2", 2, 4);
+insert into kavez values (null, "Kavez3", 1, 3);
+insert into kavez values (null, "Kavez4", 2, 4);
+insert into kavez values (null, "Kavez5", 0, 3);
+
+-- Psi u kavezima
+insert into kavez_pas values ("2019-09-09", 1, 1, null);
+insert into kavez_pas values ("2019-09-09", 1, 2, null);
+insert into kavez_pas values ("2019-09-09", 2, 3, null);
+insert into kavez_pas values ("2019-09-09", 2, 4, null);
+insert into kavez_pas values ("2019-09-09", 3, 5, null);
+insert into kavez_pas values ("2019-09-09", 3, 6, null);
+insert into kavez_pas values ("2019-09-09", 4, 7, null);
+insert into kavez_pas values ("2019-09-09", 4, 8, null);
+insert into kavez_pas values ("2019-09-09", 5, 9, null);
+insert into kavez_pas values ("2019-09-09", 5, 10, null);
+insert into kavez_pas values ("2019-09-09", 5, 14, null);
+
 
 
